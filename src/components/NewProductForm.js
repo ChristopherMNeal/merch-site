3f99e4d8cd457fafd892f52da98a237efb7c9597
Christopher Neal <christopher.m.neal@gmail.com>
2022-02-09 11:25:22 -0800

    WIP: add new product form
    Co-authored-by: Anna Pittman <annaepittman@gmail.com>
    Co-authored-by: Ella Tanttu <ellajtanttu@gmail.com>
    Co-authored-by: Christy Welch <christinamawel@gmail.com>

--- src/components/NewProductForm.js
+++ src/components/NewProductForm.js
@@ -1,2 +1,41 @@
-import React
-impor from "react";
+import React from "react";
+// import { v4 } from "uuid";
+import PropTypes from "prop-types";
+
+function NewProductForm(props){
+
+  function handleNewProductFormSubmission(event) {
+    event.preventDefault();
+    console.log({
+      names: event.target.name.value, 
+      prices: event.target.price.value,
+      quantities: event.target.quantity.value,
+      description: event.target.description.value
+    });
+  }
+
+  // event.target.names.value,
+  return (
+    <form onSubmit={handleNewProductFormSubmission}>
+      <input
+        type='text'
+        name='name'
+        placeholder='Product Name'/>
+      <label htmlFor="price">Product Price</label>
+      <input
+        type='number'
+        name='price'/>
+      <label htmlFor="quanity">Current Product Qty</label>
+      <input
+        type='number'
+        name='quantity'/>
+      <textarea
+        name='description'
+        placeholder='Product Description'/>
+      <button type='submit'>Create</button>
+    </form>
+  )
+
+}
+
+export default NewProductForm;
--- src/components/ProductController.js
+++ src/components/ProductController.js
@@ -2,6 +2,7 @@ import React from "react";
 import ProductList from "./ProductList";
 import { v4 } from "uuid";
 import ProductDetails from "./ProductDetails";
+import NewProductForm from "./NewProductForm";
 // import Product from "./Product";
 
 class ProductController extends React.Component {
@@ -31,21 +32,52 @@ class ProductController extends React.Component {
           key: v4(),
         },
       ],
-      showProductDetails: false,
+      pageShowing: 1 
     };
   }
 
+  handleClick = () => {
+    if (this.state.pageShowing < 2) {
+      this.setState(prevState => ({
+        pageShowing: 2
+      }));
+    } else {
+      this.setState(prevState => ({
+        pageShowing: 1
+      }));
+    }
+  }
+
+  /*
+Product details - 0 - button => Form(2)
+Product list page - 1 -> button => Form(2), (LATER: button=> ech product)
+New Product Form - 2 -> button => List(1)
+  */
+
+
   render() {
+    let pageShowing = this.state.pageShowing;
+    let buttonText = null;
     const currentProductList = this.state.mainProductList;
+    if (this.state.pageShowing === 1) {
+      pageShowing =  <ProductList currentProductList={currentProductList} />
+      buttonText = "Create new product"
+    } else if (this.state.pageShowing === 2) {
+      pageShowing = <NewProductForm />
+      buttonText = "Go back"
+    } else {
+      pageShowing = <ProductDetails   
+      name={currentProductList[0].name}
+      price={currentProductList[0].price}
+      quantity={currentProductList[0].quantity}
+      description={currentProductList[0].description}
+      />
+      buttonText = "Create new product"
+    }
     return (
       <React.Fragment>
-        <ProductList currentProductList={currentProductList} />
-        <ProductDetails
-          name={currentProductList[0].name}
-          price={currentProductList[0].price}
-          quantity={currentProductList[0].quantity}
-          description={currentProductList[0].description}
-        />
+        {pageShowing}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
